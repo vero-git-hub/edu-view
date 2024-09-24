f48560a1f16de60a4d27d7aabeeb0c7746b97fe9
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-09-24 13:50:07 +0300

    Initial

---
+++ src/main/java/org/example/eduview/EduViewApplication.java
@@ -0,0 +1,13 @@
+package org.example.eduview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduViewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eduview/EduViewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eduview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
